aeeef3c604657080e02e709e8592c93ac956c23c
Aiello Chan <aiello.chan@gmail.com>
2021-03-18 10:53:32 +0800

    fix(schedule): Expand schedule delay to 0-10min

--- utils/scheduler.js
+++ utils/scheduler.js
@@ -52,8 +52,8 @@ let scheduler = {
     for (let taskName of taskNames) {
       let options = tasks[taskName].options;
       let willTime = moment(randomDate(options));
-      // 任务随机 0 - 1 分钟
-      let waitTime = options.dev ? 0 : Math.floor(Math.random() * 60);
+      // 任务随机 0 - 10 分钟
+      let waitTime = options.dev ? 0 : Math.floor(Math.random() * 600);
       if (options) {
         if (options.isCircle || options.dev) {
           willTime = moment().startOf("days");
